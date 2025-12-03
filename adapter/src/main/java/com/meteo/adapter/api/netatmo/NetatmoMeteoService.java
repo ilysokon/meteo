d68d394d5ddb329252254c96d2eca4db96456ef1
ilysokon <igor.lysokon@gmail.com>
2025-12-03 12:55:27 +0100

    refactoring to remove unnecessary service wrapper.

--- adapter/src/main/java/com/meteo/adapter/api/netatmo/NetatmoMeteoService.java
+++ adapter/src/main/java/com/meteo/adapter/api/netatmo/NetatmoMeteoService.java
@@ -1,15 +1,18 @@
 package com.meteo.adapter.api.netatmo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meteo.application.ports.out.geometeo.GeometeoService;
+import com.meteo.application.ports.out.persistence.PersistenceService;
 import com.meteo.domain.model.Geometeo;
 import io.reactivex.Flowable;
 import jakarta.inject.Inject;
-import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.inject.Singleton;
+import reactor.core.publisher.Flux;
 
 import java.sql.Timestamp;
 import java.time.ZoneId;
@@ -18,7 +21,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 @Singleton
-public class NetatmoMeteoService {
+public class NetatmoMeteoService implements GeometeoService {
 	private static final Logger LOG = LoggerFactory.getLogger(NetatmoMeteoService.class);
 
     private final ObjectMapper objectMapper;
@@ -28,102 +31,80 @@ public class NetatmoMeteoService {
 
     private final NetatmoGetMeasureApiClient netatmoGetMeasureApiClient;
     private final NetatmoLowLevelGetMeasureApiClient netatmoLowLevelGetMeasureApiClient;
+    private final PersistenceService persistenceService;
+
     @Inject
     private NetatmoConfiguration netatmoConfiguration;
 
-    public NetatmoMeteoService(final NetatmoGetMeasureApiClient netatmoGetMeasureApiClient, final NetatmoLowLevelGetMeasureApiClient netatmoLowLevelGetMeasureApiClient, final ObjectMapper objectMapper) {
+    public NetatmoMeteoService(final NetatmoGetMeasureApiClient netatmoGetMeasureApiClient, final NetatmoLowLevelGetMeasureApiClient netatmoLowLevelGetMeasureApiClient, final PersistenceService persistenceService, final ObjectMapper objectMapper) {
         this.netatmoGetMeasureApiClient = netatmoGetMeasureApiClient;
         this.netatmoLowLevelGetMeasureApiClient = netatmoLowLevelGetMeasureApiClient;
+        this.persistenceService = persistenceService;
         this.objectMapper = objectMapper;
 
         LOG.info("NetatmoMeteoService is created");
 	}
 
-	//@Override
-	public Publisher<Map<Long, Double>> getMeteo2() {
-		LOG.info("Getting meteo data from Netatmo Api Client ...");
+	public void fetchMeasureAndPersist(final String deviceId, final String type, int requestNumber, int targetRequestNumber) {
+		LOG.info("Getting meteo data from Netatmo Low Level Api client for deviceId: " + deviceId + " and type: " + type);
+        initializeEndDate(requestNumber);
+        initializeBeginDate();
 
-        return Flowable.fromPublisher(netatmoGetMeasureApiClient.fetchMeasure())
-                .map(json -> {
-                    JsonNode root = objectMapper.readTree(json);
-                    JsonNode bodyNode = root.get("body");
+        LOG.info("beginDate: " + beginDate + ", endDate: " + endDate);
+        Flux.from(
+             Flowable.fromPublisher(netatmoLowLevelGetMeasureApiClient.fetchMeasure(deviceId, type, beginDate, endDate))
+                .map(json -> mapToGeometeoData(requestNumber, targetRequestNumber, json))
+                .map(result -> new Geometeo(deviceId, type, result))
+        ).subscribe(new ReactiveStreamsSubscriber(persistenceService, "no data in request: " + requestNumber + ", for deviceId " + deviceId));
+    }
 
-                    Map<Long, Double> result = new TreeMap<>();
-                    bodyNode.fields().forEachRemaining(entry -> {
-                        long ts = Long.parseLong(entry.getKey());
-                        double value = entry.getValue().get(0).asDouble();
-                        result.put(ts, value);
-                    });
 
-                    return result;
-                });
+
+    public void fetchMeasureAndPersist(final String deviceId, final String moduleId, final String type, int requestNumber, int targetRequestNumber) {
+		LOG.info("Getting meteo data from Netatmo Low Level Api client for deviceId: " + deviceId + ", modedelId: " + moduleId +  " and type: " + type);
+        initializeEndDate(requestNumber);
+        initializeBeginDate();
+
+        LOG.info("beginDate: " + beginDate + ", endDate: " + endDate);
+        Flux.from(
+              Flowable.fromPublisher(netatmoLowLevelGetMeasureApiClient.fetchMeasure(deviceId, moduleId, type, beginDate, endDate))
+                .map(json -> mapToGeometeoData(requestNumber, targetRequestNumber, json))
+                .map(result -> new Geometeo(deviceId, moduleId, type, result))
+        ).subscribe(new ReactiveStreamsSubscriber(persistenceService, "no data in request: " + requestNumber + ", for deviceId " + deviceId));
 	}
 
-    public Publisher<Geometeo> fetchMeasure(final String deviceId, final String type, int requestNumber, int targetRequestNumber) {
-		LOG.info("Getting meteo data from Netatmo Low Level Api client for deviceId: " + deviceId + " and type: " + type);
-        if (endDate == null || requestNumber == 1) {
-            endDate = getUnixLocalTimestamp();
+    private Map<Long, Double> mapToGeometeoData(int requestNumber, int targetRequestNumber, String json) throws JsonProcessingException {
+        JsonNode root = objectMapper.readTree(json);
+        JsonNode bodyNode = root.get("body");
+        Map<Long, Double> result = new TreeMap<>();
+
+        if (!bodyNode.isEmpty()) {
+            bodyNode.fields().forEachRemaining(entry -> {
+                long ts = Long.parseLong(entry.getKey());
+                double value = entry.getValue().get(0).asDouble();
+                result.put(ts, value);
+            });
+            if(requestNumber == targetRequestNumber) {
+                LOG.info("all requests are done, requestNumber: " + requestNumber + " is reached the " + targetRequestNumber);
+                beginDate = endDate;
+                endDate = null;
+            }
         }
+
+        return result;
+    }
+
+    private void initializeBeginDate() {
         if (beginDate == null) {
             beginDate = endDate - netatmoConfiguration.measureScaleMinutes() * 60;
         }
+    }
 
-        LOG.info("beginDate: " + beginDate + ", endDate: " + endDate);
-        return Flowable.fromPublisher(netatmoLowLevelGetMeasureApiClient.fetchMeasure(deviceId, type, beginDate, endDate))
-                .map(json -> {
-                    JsonNode root = objectMapper.readTree(json);
-                    JsonNode bodyNode = root.get("body");
-                    Map<Long, Double> result = new TreeMap<>();
-
-                    if (!bodyNode.isEmpty()) {
-                        bodyNode.fields().forEachRemaining(entry -> {
-                            long ts = Long.parseLong(entry.getKey());
-                            double value = entry.getValue().get(0).asDouble();
-                            result.put(ts, value);
-                        });
-                        if(requestNumber == targetRequestNumber) {
-                            LOG.info("all requests are done, requestNumber: " + requestNumber + " is reached the " + targetRequestNumber);
-                            beginDate = endDate;
-                            endDate = null;
-                        }
-                    }
-
-                    return new Geometeo(deviceId, type, result);
-                });
-	}
-
-    public Publisher<Geometeo> fetchMeasure(final String deviceId, final String moduleId, final String type, int requestNumber, int targetRequestNumber) {
-		LOG.info("Getting meteo data from Netatmo Low Level Api client for deviceId: " + deviceId + " and type: " + type);
+    private void initializeEndDate(int requestNumber) {
         if (endDate == null || requestNumber == 1) {
             endDate = getUnixLocalTimestamp();
         }
-        if (beginDate == null) {
-            beginDate = endDate - netatmoConfiguration.measureScaleMinutes() * 60;
-        }
-
-        LOG.info("beginDate: " + beginDate + ", endDate: " + endDate);
-        return Flowable.fromPublisher(netatmoLowLevelGetMeasureApiClient.fetchMeasure(deviceId, moduleId, type, beginDate, endDate))
-                .map(json -> {
-                    JsonNode root = objectMapper.readTree(json);
-                    JsonNode bodyNode = root.get("body");
-                    Map<Long, Double> result = new TreeMap<>();
-
-                    if (!bodyNode.isEmpty()) {
-                        bodyNode.fields().forEachRemaining(entry -> {
-                            long ts = Long.parseLong(entry.getKey());
-                            double value = entry.getValue().get(0).asDouble();
-                            result.put(ts, value);
-                        });
-                        if(requestNumber == targetRequestNumber) {
-                            LOG.info("all requests are done, requestNumber: " + requestNumber + " is reached the " + targetRequestNumber);
-                            beginDate = endDate;
-                            endDate = null;
-                        }
-                    }
-
-                    return new Geometeo(deviceId, moduleId, type, result);
-                });
-	}
+    }
 
     private static long getUnixLocalTimestamp() {
         Date date = new Date();
--- bootstrap/src/main/java/com/meteo/bootstrap/GeometeoJob.java
+++ bootstrap/src/main/java/com/meteo/bootstrap/GeometeoJob.java
@@ -3,7 +3,7 @@ package com.meteo.bootstrap;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.meteo.adapter.reactivestreams.ReactiveGeometeoService;
+import com.meteo.adapter.api.netatmo.NetatmoMeteoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +13,12 @@ import jakarta.inject.Singleton;
 
 @Singleton
 public class GeometeoJob {
-	private final ReactiveGeometeoService reactiveGeometeoService;
+	private final NetatmoMeteoService netatmoMeteoService;
 	private static final Logger LOG = LoggerFactory.getLogger(GeometeoJob.class);
 
 	@Inject()
-	public GeometeoJob(final ReactiveGeometeoService reactiveGeometeoService) {
-		this.reactiveGeometeoService = reactiveGeometeoService;
+	public GeometeoJob(final NetatmoMeteoService netatmoMeteoService) {
+		this.netatmoMeteoService = netatmoMeteoService;
 		LOG.info("GeometeoJob created");
 	}
 
@@ -26,24 +26,24 @@ public class GeometeoJob {
 	void fetchMeasure() {
 		LOG.info("Simple Job is triggered every 1 minutes: {}", new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date()));
 		// "NAMain", Living room
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "temperature", 1, 13);
-		reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "pressure", 2, 13);
-		reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "humidity", 3, 13);
-		reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "CO2", 4, 13);
-		reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "noise", 5, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "temperature", 1, 13);
+		netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "pressure", 2, 13);
+		netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "humidity", 3, 13);
+		netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "CO2", 4, 13);
+		netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "noise", 5, 13);
 
         // "NAModule1", "Outdoor"
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a","02:00:00:84:50:66", "temperature", 6, 13);
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a","02:00:00:84:50:66", "humidity", 7, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a","02:00:00:84:50:66", "temperature", 6, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a","02:00:00:84:50:66", "humidity", 7, 13);
 
         // "NAModule4", "Bedroom"
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:c3:ee", "temperature", 8, 13);
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:c3:ee", "humidity", 9, 13);
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:c3:ee", "CO2", 10, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:c3:ee", "temperature", 8, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:c3:ee", "humidity", 9, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:c3:ee", "CO2", 10, 13);
 
         // "NAModule4", "Baby room"
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:11:9c", "temperature", 11, 13);
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:11:9c", "humidity", 12, 13);
-        reactiveGeometeoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:11:9c", "CO2", 13, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:11:9c", "temperature", 11, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:11:9c", "humidity", 12, 13);
+        netatmoMeteoService.fetchMeasureAndPersist("70:ee:50:84:33:6a", "03:00:00:07:11:9c", "CO2", 13, 13);
 	}
 }
